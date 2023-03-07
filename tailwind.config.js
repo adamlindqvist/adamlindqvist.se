e66ae816f7a9209639fce67edb5807d9ae471cc0
Adam Lindqvist <adam.lindqvist@omegapoint.se>
2023-03-07 11:42:16 +0100

    New website

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      sans: 'Inter, "Helvetica Neue", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
+  },
+  plugins: [],
+};
