75ef8d0e63f06155f344daac73ff5acc55ba8c1f
Adam Lindqvist <adam.lindqvist@omegapoint.se>
2023-03-09 10:12:38 +0100

    Update fallback fonts to improve CLS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
   theme: {
     fontFamily: {
-      sans: 'Inter, "Helvetica Neue", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [],
